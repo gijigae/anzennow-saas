08261de6978315af0b1cd48088e37ec214bf3604
sangmin <gijigae@gmail.com>
2025-01-27 23:41:57 +0900

    WIP: use clerk for auth

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -6,7 +6,7 @@ export default async function SettingsPage() {
   const user = await getUser();
 
   if (!user) {
-    redirect('/sign-in');
+    redirect('/login');
   }
 
   const teamData = await getTeamForUser(user.id);
